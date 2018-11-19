e879545a8b91e8b205e6bec8bbb289bf034867bd
Garder94 <tgarder@hotmail.com>
2018-11-19 19:52:36 +0100

    JADA

---
+++ camera.py
@@ -0,0 +1,28 @@
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video1 = cv2.VideoCapture(0)
+        self.video2 = cv2.VideoCapture(1)
+        
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+    
+    def __del__(self):        
+        self.video1.release()
+        self.video2.release()
+    
+    def get_frame(self):        
+        success1, image1 = self.video1.read()
+        success2, image2 = self.video2.read()
+        final = cv2.hconcat([image1, image2])    
+        resize = cv2.resize(final,None, fx=0.8, fy=0.8)
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpeg = cv2.imencode('.jpg', resize)
+        return jpeg.tobytes()
