7c076b5b697e5f4f704538a5ac1afc52d58814ca
Garder94 <tgarder@hotmail.com>
2018-11-20 16:50:29 +0100

    Added cam

--- camera.py
+++ camera.py
@@ -7,8 +7,8 @@ class VideoCamera(object):
         # from a webcam, comment the line below out and use a video file
         # instead.
         self.video = cv2.VideoCapture(0)
-        self.video1 = cv2.VideoCapture(1)
-        self.video2 = cv2.VideoCapture(2)
+        self.video1 = cv2.VideoCapture(3)
+        #self.video2 = cv2.VideoCapture(2)
         #self.video3 = cv2.VideoCapture(3)
         
         # If you decide to use video.mp4, you must have this file in the folder
@@ -18,15 +18,14 @@ class VideoCamera(object):
     def __del__(self):        
         self.video.release()
         self.video1.release()
-        self.video2.release()
         #self.video3.release()
     
     def get_frame(self):        
         success, image = self.video.read()
         success1, image1 = self.video1.read()
-        success2, image2 = self.video2.read()
+        #success2, image2 = self.video2.read()
         #success3, image3 = self.video3.read()
-        final = cv2.vconcat([image, image1])
+        final = cv2.hconcat([image, image1])
         #final1 = cv2.hconcat([image2, image3])   
          
         #final2 = np.concatenate((final, image2), axis=0)
