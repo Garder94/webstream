57c749def6253a465e8b50fde2b0c11e8bd1fc47
Garder94 <tgarder@hotmail.com>
2018-11-20 13:07:51 +0100

    added cameras

--- camera.py
+++ camera.py
@@ -8,7 +8,7 @@ class VideoCamera(object):
         self.video = cv2.VideoCapture(0)
         self.video1 = cv2.VideoCapture(1)
         self.video2 = cv2.VideoCapture(2)
-        self.video3 = cv2.VideoCapture(3)
+        #self.video3 = cv2.VideoCapture(3)
         
         # If you decide to use video.mp4, you must have this file in the folder
         # as the main.py.
@@ -18,16 +18,16 @@ class VideoCamera(object):
         self.video.release()
         self.video1.release()
         self.video2.release()
-        self.video3.release()
+        #self.video3.release()
     
     def get_frame(self):        
         success, image = self.video.read()
         success1, image1 = self.video1.read()
         success2, image2 = self.video2.read()
-        success3, image3 = self.video3.read()
+        #success3, image3 = self.video3.read()
         final = cv2.hconcat([image, image1])
-        final1 = cv2.hconcat([image2, image3])    
-        final2 = cv2.vconcat([final, final1])
+        #final1 = cv2.hconcat([image2, image3])    
+        final2 = cv2.vconcat([final, image2])
         resize = cv2.resize(final2,None, fx=0.8, fy=0.8)
         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
         # so we must encode it into JPEG in order to correctly display the
